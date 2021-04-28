4c2fadcac771aade6bcc71503469bf9debb12104
Manzur Khan Sarguru <manzurkds@gmail.com>
2021-04-28 14:45:26 +0530

    chore: Updated symptoms options (#5)

--- XState-Chatbot/nodejs/src/machine/messages/triage.js
+++ XState-Chatbot/nodejs/src/machine/messages/triage.js
@@ -41,7 +41,7 @@ let messages = {
   },
   symptoms: {
     prompt: {
-      en_IN: 'Thanks for this information {{name}}! Do you have one or more of these complaints?: \n- Fever\n- Cough with or without spit/Sore throat\n- Loss of smell/taste\n- Runny nose\n- Difficulty breathing/Persistent pain/pressure in the chest\n- Muscle pain\n- Nausea & loose motions\n- Bluish discolorations of lips/face\n- Inability to eat / drink / walk\n'
+      en_IN: 'Thanks for this information {{name}}! Do you have one or more of these complaints?: \n- Fever\n- Cough with or without spit/Sore throat\n- Loss of smell/taste\n- Runny nose\n- Difficulty breathing/Persistent pain/pressure in the chest\n- Muscle pain\n- Nausea & loose motions\n'
     }
   },
   rtpcr: {
